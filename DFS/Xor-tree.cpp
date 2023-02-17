ca629fd8316dcc9a28d0ff15e4d23232544dfdcf
Mohamed Abdalrazek <91884590+lfg6f4w@users.noreply.github.com>
2023-02-17 13:41:11 +0200

    https://codeforces.com/contest/429/problem/A

--- DFS/Xor-tree.cpp
+++ DFS/Xor-tree.cpp
@@ -109,3 +109,98 @@ int main() {
         doWork(i), cout << ed;
     Good_Bay 0;
 }
+// ------------------------------------------------------------------------------------------
+
+/*** وَمَا تَوْفِيقِي إِلَّا بِاللَّهِ ۚ عَلَيْهِ تَوَكَّلْتُ وَإِلَيْهِ أُنِيبُ ***/
+
+#include                             <bits/stdc++.h>
+#define     ll                       long long
+#define     ld                       long double
+#define     ft                       first
+#define     sd                       second
+#define     ed                       '\n'
+#define     sz(x)                    x.size()
+#define     all(x)                   x.begin(), x.end()
+#define     rall(x)                  x.rbegin(), x.rend()
+#define     fixed(x)                 fixed<<setprecision(x)
+#define     memo(dp,x)               memset(dp,x,sizeof(dp))
+#define     sumOf(a)                 (ll)((-1 + sqrt(8 * a + 1)) / 2)
+#define     Good_Bay                 return
+
+const int mod = 1000000007, OO = 2e9, EPS = 1e-7;
+const double pi = acos(-1);
+
+using namespace std;
+
+template < typename T > istream& operator >> (istream& in, vector < T >& v) {
+    for (auto& x : v) in >> x;
+    return in;
+}
+
+template < typename T > ostream& operator << (ostream& out, const vector < T >& v) {
+    for (const T& x : v) out << x << ' ';
+    return out;
+}
+
+void Accept() { // for VS code
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int dxy[9][2] = { {-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{0,1},{-1,1},{-1,0} };
+
+int mul_inv(int a, int b) { // don't forget it (Puiple(-40))
+    int b0 = b, t, q;
+    int x0 = 0, x1 = 1;
+    if (b == 1) return 1;
+    while (a > 1) {
+        q = a / b;
+        t = b, b = a % b, a = t;
+        t = x0, x0 = x1 - q * x0, x1 = t;
+    }
+    if (x1 < 0) x1 += b0;
+    return x1;
+}
+void doWork(ll tt) {
+    int n;
+    cin >> n;
+    vector<int>eg[n + 1], init(n), goal(n), ans;
+    for (int i = 1;i < n;i++) {
+        int u, v;
+        cin >> u >> v;
+        eg[u].push_back(v);
+        eg[v].push_back(u);
+    }
+    cin >> init >> goal;
+    function<void(int, int, bool, bool, bool)>dfs = [&](int node, int parent, int level, int even, int odd) {
+        if (level)init[node - 1] = (odd ? !init[node - 1] : init[node - 1]);
+        else init[node - 1] = (even ? !init[node - 1] : init[node - 1]);
+        if (init[node - 1] != goal[node - 1]) {
+            init[node - 1] = goal[node - 1];
+            ans.push_back(node);
+            if (level)odd = !odd;
+            else even = !even;
+        }
+        for (int i : eg[node]) {
+            if (i != parent)
+                dfs(i, node, !level, even, odd);
+        }
+    };
+    dfs(1, -1, 0, 0, 0);
+    cout << ans.size() << ed;
+    for (auto i : ans)cout << i << ed;
+}
+int main() {
+    Accept();
+    int t = 1;
+    // cin >> t;
+    for (int i = 1;i <= t;i++)
+        // cout << "Case "<<i<<": ",
+        doWork(i);// cout << ed;
+    Good_Bay 0;
+}
