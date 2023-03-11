389e9b353bddd433d1d45452941eba48be01f998
Mohamed Abdalrazek <91884590+lfg6f4w@users.noreply.github.com>
2023-03-11 22:49:23 +0200

    Create Weekend.cpp

---
+++ Floyd/Weekend.cpp
@@ -0,0 +1,83 @@
+//   https://codeforces.com/group/yAhl7s6c8Y/contest/393344/problem/H
+
+
+/*** وَمَا تَوْفِيقِي إِلَّا بِاللَّهِ ۚ عَلَيْهِ تَوَكَّلْتُ وَإِلَيْهِ أُنِيبُ ***/
+#include                             <bits/stdc++.h>
+#define     ll                       long long
+#define     ld                       long double
+#define     ed                       '\n'
+#define     sz(x)                    x.size()
+#define     all(x)                   x.begin(), x.end()
+#define     rall(x)                  x.rbegin(), x.rend()
+#define     fixed(x)                 fixed<<setprecision(x)
+#define     memo(dp,x)               memset(dp,x,sizeof(dp))
+#define     sumOf(a)                 (ll)((-1 + sqrt(8 * a + 1)) / 2)
+#define     Good_Bay                 return
+
+const int mod = 1e9 + 7, oo = 0x3f3f3f3f, MAX = 2e5 + 5;
+const double pi = acos(-1), EPS = 1e-7;
+const ll OO = LLONG_MAX;
+
+using namespace std;
+
+template < typename T > istream& operator >> (istream& in, vector < T >& v) {
+    for (auto& x : v) in >> x;
+    return in;
+}
+
+template < typename T > ostream& operator << (ostream& out, const vector < T >& v) {
+    for (const T& x : v) out << x << ' ';
+    return out;
+}
+
+void Accept() { // for VS code
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+int dy[] = { 1 , 0 , -1 ,  0 , 1 , -1 ,  1 , -1 };
+int dx[] = { 0 , 1 ,  0 , -1 , 1 , -1 , -1 ,  1 };
+void doWork(ll tt) {
+    int m, n, f;
+    cin >> n >> m >> f;
+    vector<vector<int>>dist(n + 1, vector<int>(n + 1, oo));
+    for (int i = 0;i <= n;i++)dist[i][i] = 0;
+    for (int i = 0;i < m;++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        dist[u][v] = w;
+        dist[v][u] = w;
+    }
+    vector<int>v(f);
+    cin >> v;
+    sort(all(v));
+    for (int k = 1;k <= n;k++)
+        for (int i = 1;i <= n;i++)
+            for (int j = 1;j <= n;j++)
+                if(dist[i][k] != oo && dist[k][j] != oo)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    int ans = oo;
+    do {
+        int x = dist[1][v[0]] + dist[v[f - 1]][n];
+        for (int i = 0;i + 1 < f;i++)
+            x += dist[v[i]][v[i + 1]];
+        ans = min(x, ans);
+    } while (next_permutation(all(v)));
+    cout << "Case " << tt << ": "<< ans;
+}
+int main() {
+    Accept();
+    int t = 1;
+    cin >> t;
+    for (int i = 1;i <= t;i++) {
+        // cout << "Case "<<i<<": ",
+        doWork(i), cout << ed;
+     //   if (i < t)cout << ed;
+    }
+    Good_Bay 0;
+}
+ 
